44e41f0e549d5eb28d3a85a20812601c142d0c69
ilyoil2 <0211ilyoil@gmail.com>
2026-06-29 23:05:42 +0900

    fix: 컬럼 정리

--- conversion_yanki/cleanup_data3.sql
+++ conversion_yanki/cleanup_data3.sql
@@ -21,3 +21,8 @@ WHERE id NOT IN (
     SELECT MIN(id) FROM tbl_content_word
     GROUP BY surface, word_type, reading, pos, jlpt_level
 );
+
+-- meaning_ko 수정
+UPDATE tbl_content_kanji SET meaning_ko = '①반대 반 · 배반 반 ②젖힐 반 ③돌이킬 반 ④ 반복할 반' WHERE character = '反';
+UPDATE tbl_content_kanji SET meaning_ko = '①무리 도 · 제자 도 ②걸을 도 ③맨손 도 ④ 헛될 도' WHERE character = '徒';
+UPDATE tbl_content_kanji SET meaning_ko = '①변별할 변 ②말할 변 ③대신할 변 ④ 갖출 판' WHERE character = '弁';
