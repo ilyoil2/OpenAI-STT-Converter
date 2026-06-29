e292de7ce0d74eb5e48092309e4778258c19872d
ilyoil2 <0211ilyoil@gmail.com>
2026-06-29 23:57:04 +0900

    chore: 데이터 수정

--- conversion_yanki/cleanup_data3.sql
+++ conversion_yanki/cleanup_data3.sql
@@ -16,11 +16,15 @@ WHERE word_id IN (
     )
 );
 
+-- wordmeaning 정리 후 word 본체도 같은 기준으로 중복 제거
 DELETE FROM tbl_content_word
 WHERE id NOT IN (
     SELECT MIN(id) FROM tbl_content_word
     GROUP BY surface, word_type, reading, pos, jlpt_level
 );
+
+-- on_reading의 * 제거
+UPDATE tbl_content_kanji SET on_reading = REPLACE(on_reading, '*', '') WHERE on_reading LIKE '%*%';
 
 -- meaning_ko 수정
 UPDATE tbl_content_kanji SET meaning_ko = '①반대 반 · 배반 반 ②젖힐 반 ③돌이킬 반 ④ 반복할 반' WHERE character = '反';
